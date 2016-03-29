e3b51964597aba29125e956de1fc5ff420edba2a
Evan Schwartz <evan.mark.schwartz@gmail.com>
2016-03-28 17:24:29 -0700

    make test seeder and downloader configurable

--- downloader.js
+++ downloader.js
@@ -2,21 +2,19 @@
 
 const WebTorrentIlp = require('./index')
 const debug = require('debug')('WebTorrentIlp:downloader')
+const crypto = require('crypto')
 
 const leecher = new WebTorrentIlp({
-  address: 'alice@blue.ilpdemo.org',
-  password: 'alice',
-  publicKey: 'lfFMEl9mWw56HygZGYejElw64wnKschRQSzu+JuZkVw=',
-  price: '0.00000001'
+  address: process.env.ADDRESS || 'alice@blue.ilpdemo.org',
+  password: process.env.PASSWORD || 'alice',
+  publicKey: crypto.randomBytes(32).toString('base64'),
+  price: process.env.PRICE || '0.00000000001'
 })
 
-const magnetURI = 'magnet:?xt=urn:btih:eca3080363229696b44f99f12e1cab902965777d&dn=interledger.pdf&tr=http%3A%2F%2Flocalhost%3A8000%2Fannounce'
+const magnetURI = process.argv.length > 2 ? process.argv[2] : 'magnet:?xt=urn:btih:eca3080363229696b44f99f12e1cab902965777d&dn=interledger.pdf&tr=http%3A%2F%2Flocalhost%3A8000%2Fannounce'
 
 const leecherTorrent = leecher.add(magnetURI, {
   announceList: [['http://localhost:8000/announce']]
-})
-leecherTorrent.on('download', function (chunkSize) {
-  debug('leecher downloaded ' + chunkSize)
 })
 leecherTorrent.on('done', function () {
   debug('leecher done, downloaded ' + leecherTorrent.files.length + ' files')
--- seeder.js
+++ seeder.js
@@ -2,15 +2,18 @@
 
 const WebTorrentIlp = require('./index')
 const debug = require('debug')('WebTorrentIlp:seeder')
+const crypto = require('crypto')
 
 const seeder = new WebTorrentIlp({
-  address: 'walt@red.ilpdemo.org',
-  password: 'walt',
-  publicKey: '7cLvHbeOmx4TGZovRInmw37xSGHm6P96VM+Ng5z0+C8=',
-  price: '0.00000001'
+  address: process.env.ADDRESS || 'walt@red.ilpdemo.org',
+  password: process.env.PASSWORD || 'walt',
+  publicKey: crypto.randomBytes(32).toString('base64'),
+  price: process.env.PRICE || '0.00000000001'
 })
 
-const seederTorrent = seeder.seed('/Users/eschwartz/Downloads/interledger.pdf', {
+const file = process.argv.length > 2 ? process.argv[2] : '/Users/eschwartz/Downloads/interledger.pdf'
+
+const seederTorrent = seeder.seed(file, {
   announceList: [['http://localhost:8000/announce']],
   private: true
 })
